89eebba51a7ca5f516ff98559f3b214e1cf4979d
Hugo Lundqvist <hugo@basic.nu>
2019-02-22 17:52:38 +0100

    add message window when pressing submit in the form.

---
+++ alert-message.js
@@ -0,0 +1,25 @@
+;(function() {
+  const submitButton = document.getElementById('alertMessageButton')
+  const alertMsg = document.querySelector('.alertMsg')
+  submitButton.addEventListener('click', showMessage)
+
+  function showMessage() {
+    if (alertMsg.getAttribute('id') === 'hidden') {
+      alertMsg.setAttribute('id', '')
+      document.querySelector('.alertTextbox').innerHTML = messageContent()
+    } else {
+      alertMsg.setAttribute('id', 'hidden')
+    }
+  }
+
+  function messageContent() {
+    let fullFormData = Array.from(document.querySelectorAll('.formInput'))
+    let inputInfoArr = fullFormData.map(extractString)
+    return inputInfoArr.join('<br>')
+  }
+
+  function extractString(formObj) {
+    console.log('working')
+    return `${formObj.placeholder}: ${formObj.value}`
+  }
+})()
